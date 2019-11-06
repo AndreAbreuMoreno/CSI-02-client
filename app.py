d0af9520525b5cb3a3b28a766c2cd5731a336444
Heladio Lopes <heladio.lopes@gmail.com>
2019-11-06 15:38:33 -0300

    Usando api

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from flask import render_template
+import json
+import requests
 
 app = Flask(__name__)
+
+
+@app.route('/cidade/<nome_da_cidade>')
+def cidade(nome_da_cidade):
+
+    req = requests.get('http://pibdascidades.herokuapp.com/pib?cidade={}'.format(nome_da_cidade))
+    city = json.loads(req.content.decode())
+
+    return render_template('cidades.html', city=city)
 
 
 @app.route('/')
