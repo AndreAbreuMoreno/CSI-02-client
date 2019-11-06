aaf4dab3e6f7d60ebfe3c7d93eeb64217717006c
Heladio Lopes <heladio.lopes@gmail.com>
2019-11-06 15:05:07 -0300

    Primeira tela

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', hello='Hello World')
 
 
 if __name__ == '__main__':
