59516ef14aed90e0efa6d598be243343ab2dac61
Beethoven Brandao <beethovenbrandaofilho@gmail.com>
2019-11-06 16:14:40 -0300

    Template para usuário

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 
+from flask import request, redirect, url_for
+
 app = Flask(__name__)
 
 
@@ -9,5 +11,37 @@ def index():
     return render_template('index.html', hello='Hello World')
 
 
+@app.route('/city-info/<language>')
+def city_info(language):
+    return render_template('city_info.html', language=language)
+
+
+@app.route('/city-name', methods=['GET', 'POST'])
+def city_name():
+    if request.method == 'POST':
+        language = request.form.get('language')
+        return redirect(url_for('city_info', language=language))
+    return render_template('city_name.html')
+
+
 if __name__ == '__main__':
     app.run()
+
+
+"""
+EXAMPLE
+@app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
+def form_example():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        language = request.form.get('language')
+        framework = request.form['framework']
+
+        return '''<h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+
+    return '''<form method="POST">
+                  Language: <input type="text" name="language"><br>
+                  Framework: <input type="text" name="framework"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+"""
